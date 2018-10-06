41a8b8af0f52f013d98b1b96bb3d9a1213fefbc2
HuangShao1314 <1052912975@qq.com>
2018-10-06 16:58:19 +0800

    first commit

---
+++ src/views/login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <lang-select class="set-language"/>
+  </div>
+</template>
+
+<script>
+import LangSelect from '@/components/LangSelect'
+export default {
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+.set-language {
+      color: #fff;
+      position: absolute;
+      top: 5px;
+      right: 0px;
+    }
+</style>
